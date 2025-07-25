66786b22e42ad480580831b1b7cc41aaf4576044
Shobayo Samuel <shobayosamuel62@gmail.com>
2025-07-25 23:51:28 +0100

    feat: user-service

--- services/user-service/internal/service/user-service.go
+++ services/user-service/internal/service/user-service.go
@@ -21,4 +21,55 @@ type UserProfileService interface {
 	UpdateLocation(userID uuid.UUID, lat, lng float64) error
 }
 
-func NewUserProfileService(userProfileRepo repository.UserProfileRepository)
+func NewUserProfileService(userProfileRepo repository.UserProfileRepository) UserProfileService {
+	return &userProfileService{
+		userProfileRepo: userProfileRepo,
+	}
+}
+
+func (s *userProfileService) GetProfile(userID uuid.UUID) (*models.UserProfile, error) {
+	return s.GetProfile(userID)
+}
+
+func (s *userProfileService) UpdateProfile(userID uuid.UUID, updateData utils.UpdateProfileRequest) (*models.UserProfile, error) {
+	profile, err := s.userProfileRepo.GetProfile(userID)
+	if err != nil {
+		// create a new profile if itis non-existent
+		profile = &models.UserProfile{
+			UserID: userID,
+		}
+	}
+	if updateData.Name != "" {
+		profile.Name = updateData.Name
+	}
+	if updateData.Bio != "" {
+		profile.Bio = updateData.Bio
+	}
+	if updateData.Age > 0 {
+		profile.Age = updateData.Age
+	}
+	if updateData.Gender != "" {
+		profile.Gender = updateData.Gender
+	}
+	if len(updateData.Interests) > 0 {
+		profile.Interests = updateData.Interests
+	}
+	if profile.ID == uuid.Nil {
+		err = s.userProfileRepo.CreateProfile(profile)
+	} else {
+		err = s.userProfileRepo.UpdateProfile(profile)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return profile, nil
+}
+
+func (s *userProfileService) AddPhoto(userID uuid.UUID, photoURL string) error {
+	profile, err := s.userProfileRepo.GetProfile(userID)
+	if err != nil {
+		return err
+	}
+	if len(proflle.Photos)
+}
